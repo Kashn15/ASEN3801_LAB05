56c9f0bee3713d6c7474d686624356a6fe884a04
Sayer-Gage <148849939+Sayer-Gage@users.noreply.github.com>
2025-11-18 09:27:58 -0700

    Update AircraftEOM.m

--- Lab 05/AircraftEOM.m
+++ Lab 05/AircraftEOM.m
@@ -16,7 +16,8 @@ wE = aircraft_state(9);
 p = aircraft_state(10); 
 q = aircraft_state(11); 
 r = aircraft_state(12);
-
+ap = aircraft_parameters;
+I = [ap.Ix,0,ap.Ixz;0,ap.Iy,0;0,0,ap.Iz];
 % Classifying certain Parameters
 omega = [p;q;r]; % Body Frame
 Vb = [uE;vE;wE];
@@ -27,20 +28,20 @@ R_BE = Rot_Mat(phi,theta,psi);    % Body to Earth
 R_EB = R_BE.';                    % Earth to Body
 
 % Aerodynamic Forces and Moments
-[Aero_F, Aero_M] = AeroForcesAndMoments(aircraft_state, aircraft_surfaces, wind_inertial, density, aircraft_parameters);
+[Aero_F, Aero_M] = AeroForcesAndMoments(aircraft_state, aircraft_surfaces, wind_inertial, aircraft_parameters);
 
 % Calculate thrust and gravitational forces
-F_thrust = [0; 0; aircraft_parameters.Zc]; % rotor resultant along body z
+% rotor resultant along body z
 F_g = aircraft_parameters.m * (R_BE * [0; 0; aircraft_parameters.g]); % gravity expressed in body
-F_tot = F_thrust + F_g + Aero_F; % Total forces
-M_tot = [aircraft_parameters.Lc; aircraft_parameters.Mc; aircraft_parameters.Nc] + Aero_M; % Total moments
+F_tot = F_g + Aero_F; % Total forces
+M_tot = Aero_M; % Total moments
 
 % Kinematics
 position_dot = R_EB * Vb;
 eul_dot = euler_kinematics(phi,theta,psi,omega);
 
 % Dynamics
-uvw_dot  = (F_tot - cross(omega, m * Vb)) / m; % Cross() is MatLab's cross function
+uvw_dot  = (F_tot - cross(omega, ap.m * Vb)) / ap.m; % Cross() is MatLab's cross function
 pqr_dot  = I \ (M_tot - cross(omega, I*omega));
 
 xdot = [position_dot; eul_dot; uvw_dot; pqr_dot];
