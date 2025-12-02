8140aa2779e3dfbd0c65257419b041174a8a532e
Natsumi Kakuda <aska7164@colorado.edu>
2025-12-02 10:14:51 -0700

    Update AircraftEOM.m

--- Lab 05/AircraftEOM.m
+++ Lab 05/AircraftEOM.m
@@ -1,5 +1,5 @@
 %% AircraftEOM Function
-% Creator: Natsumi Kakuda
+% Creators: Bridger, Natsumi, Drake, and Sayer
 
 function xdot = AircraftEOM(time, aircraft_state, aircraft_surfaces, wind_inertial, aircraft_parameters)
 
@@ -21,8 +21,25 @@ p = aircraft_state(10);
 q = aircraft_state(11); 
 r = aircraft_state(12);
 
+% Inertia
+Ix = aircraft_parameters.Ix;
+Ixz = aircraft_parameters.Ixz;
+Iy = aircraft_parameters.Iy;
+Iz = aircraft_parameters.Iz;
+
 % Inertia Matrix Set up
-I = [aircraft_parameters.Ix, 0, -aircraft_parameters.Ixz; 0, aircraft_parameters.Iy, 0; -aircraft_parameters.Ixz, 0, aircraft_parameters.Iz];
+% I = [Ix, 0, Ixz; 0, Iy, 0; Ixz, 0, Iz];
+
+% Gamma 
+Gamma = Ix*Iz -Ixz^2;
+Gamma1 = (Ixz*(Ix-Iy)+Ixz^2)/Gamma;
+Gamma2 = (Iz*(Iz-Iy)+Ixz^2)/Gamma;
+Gamma3 = Iz / Gamma;
+Gamma4 = Ixz/Gamma;
+Gamma5 = (Ix - Iz) / Gamma;
+Gamma6 = Ixz / Iy;
+Gamma7 = (Ix*(Ix-Iy)+Ixz^2)/Gamma;
+Gamma8 = Ix/Gamma;
 
 % Omega and Vb Set up
 omega = [p;q;r]; % Body Frame
@@ -30,7 +47,7 @@ Vb = [u;v;w];
 
 % Rotation Matrices
 R_BE = Rot_Mat(phi,theta,psi);    % Body to Earth
-R_EB = R_BE.';                    % Earth to Body
+% R_EB = R_BE.';                    % Earth to Body
 
 % Density Calculation
 h = -zE; % We want magnitude since our frame defines zE as positive downwards
@@ -39,21 +56,43 @@ density = rho;
 
 % Aerodynamic Forces and Moments
 [Aero_F, Aero_M] = AeroForcesAndMoments(aircraft_state, aircraft_surfaces, wind_inertial, density, aircraft_parameters);
+X = Aero_F(1);
+Y = Aero_F(2);
+Z = Aero_F(3);
+L = Aero_M(1);
+M = Aero_M(2);
+N = Aero_M(3);
 
 % Calculate thrust and gravitational forces
-F_g = aircraft_parameters.m * (R_EB * [0; 0; aircraft_parameters.g]);
-F_tot = Aero_F + F_g; % Total forces
-M_tot = Aero_M; % Total moments
+% F_g = aircraft_parameters.m * (R_EB * [0; 0; aircraft_parameters.g]);
+% F_tot = Aero_F + F_g; % Total forces
+% M_tot = Aero_M; % Total moments
 
 % Kinematics
 pos_dot = R_BE * Vb;
 eul_dot = euler_kinematics(phi,theta,psi,omega);
 
+% Extracting Velocity
+u_E = pos_dot(1);
+v_E = pos_dot(2);
+w_E = pos_dot(3);
+
 % Dynamics
-uvw_dot  = (F_tot - cross(omega, aircraft_parameters.m * Vb)) / aircraft_parameters.m; % Cross() is MatLab's cross function
-pqr_dot  = I \ (M_tot - cross(omega, I*omega));
+% uvw_dot  = (F_tot - cross(omega, aircraft_parameters.m * Vb)) / aircraft_parameters.m; % Cross() is MatLab's cross function
+% pqr_dot  = I \ (M_tot - cross(omega, I*omega));
+
+p_dot = Gamma1*p*q - Gamma2*q*r+Gamma3*L+Gamma4*N;
+q_dot = Gamma5*p*r - Gamma6*((p^2)-(r^2))+(1/Iy)*M;
+r_dot = Gamma7*p*q - Gamma1*q*r + Gamma4*L+Gamma8*N;
+pqr_dot = [p_dot; q_dot; r_dot];
+
+u_dot = (r*u_E-1*w_E)+ aircraft_parameters.g *(-sin(theta))+ (X/aircraft_parameters.m);
+v_dot = (p*w_E + q*u_E) + aircraft_parameters.g * cos(theta) * sin(phi) + (Y/aircraft_parameters.m);
+w_dot = (q*v_E - r*u_E) + aircraft_parameters.g * cos(theta) * cos(phi) + (Z/aircraft_parameters.m);
+uvw_dot = [u_dot; v_dot; w_dot];
 
 xdot = [pos_dot; eul_dot; uvw_dot; pqr_dot];
+
 
 
 
